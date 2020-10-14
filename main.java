81385253374bd597f1dbdd380e6429a6bb174471
rene.kasetalu <rene.kasetalu@khk.ee>
2020-10-14 13:55:25 +0300

    Ylesanne 30

--- main.java
+++ main.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-            int number = 2;
-            while(number <= 100){
-            if(number % 2 == 0){
-                System.out.println(number);
-            }
-            
-            number++;
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Up to what number? ");
+        int upperNumber = Integer.parseInt(reader.nextLine());
+        int currentNumber = 1;
+        while (currentNumber <= upperNumber){
+            System.out.println(currentNumber);
+            currentNumber++;
         }
     }
 }
