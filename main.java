df87be1544137d425dbd5027c9c43c58268af2e0
rene.kasetalu <rene.kasetalu@khk.ee>
2020-10-14 12:04:23 +0300

    Ylesanne 10

--- main.java
+++ main.java
@@ -4,11 +4,9 @@ public class main {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Type a number: ");
-        int firstNumber = Integer.parseInt(reader.nextLine());
-        System.out.print("Type another number: ");
-        int secondtNumber = Integer.parseInt(reader.nextLine());
-        double result = (double)firstNumber / secondtNumber;
-        System.out.println("Division: " + firstNumber + " / " + secondtNumber + " = " + result);
+        System.out.print("Type the radius: ");
+        int radius = Integer.parseInt(reader.nextLine());
+        double circumference = 2 * Math.PI * radius;
+        System.out.println("Circumference of the circle: " + circumference);
     }
 }
