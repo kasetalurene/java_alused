39bc1258c28e7c20b893b9f259ce96f3d9150fec
rene.kasetalu <rene.kasetalu@khk.ee>
2020-10-14 11:54:02 +0300

    Ylesanne 5

--- main.java
+++ main.java
@@ -1,6 +1,12 @@
 public class main {
+
     public static void main(String[] args) {
         System.out.println("Rene Kasetalu");
+        int days = 365;
+        int hours = 24;
+        int minutes = 60;
+        int seconds = 60;
+        int secondsInAYear = days * hours * minutes * seconds;
+        System.out.println("There are " + secondsInAYear + " seconds in a year.");
     }
-    
 }
