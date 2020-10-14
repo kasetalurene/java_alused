e9d7f3c471b43939391ff7db6558fc1813a9aea3
rene.kasetalu <rene.kasetalu@khk.ee>
2020-10-14 12:11:30 +0300

    Ylesanne 14

--- main.java
+++ main.java
@@ -4,9 +4,12 @@ public class main {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Type the radius: ");
-        int radius = Integer.parseInt(reader.nextLine());
-        double circumference = 2 * Math.PI * radius;
-        System.out.println("Circumference of the circle: " + circumference);
+        System.out.print("Type a number: ");
+        int number = Integer.parseInt(reader.nextLine());
+        if(number >= 0) {
+            System.out.println("The number is positive.");
+        } else {
+            System.out.println("The number is not positive.");
+        }
     }
 }
