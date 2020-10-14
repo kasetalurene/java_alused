264600b5d0666e23fbcf7396d0d83e84803d6383
rene.kasetalu <rene.kasetalu@khk.ee>
2020-10-14 14:46:39 +0300

    Ylesanne 41

--- main.java
+++ main.java
@@ -2,18 +2,28 @@ import java.util.Scanner;
 
 public class main {
 
-        private static void printStars(int amount) {
-            int i = 1;
-            while (i <= amount){
-                System.out.print("*");
-                i++;
-            }
-            System.out.println();
-        }
-        public static void main(String[] args) {
-            printStars(5);
-            printStars(3);
-            printStars(9);
+    private static boolean drawNumber(int guess, int number, int count) {
+        if(guess < number){
+            System.out.println("The number is greater, guesses made: " + count);
+            return true;
+        } else if(guess > number){
+            System.out.println("The number is lesser, guesses made: " + count);
+            return true;
+        } else {
+            System.out.println("Congratulations, your guess is correct!");
+            return false;
         }
     }
-    
+    public static void main(String[] args) {
+        int number = (int)(Math.random() * 101);
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Guess a number: ");
+        int guess = Integer.parseInt(reader.nextLine());
+        int count = 1;
+        while(drawNumber(guess, number, count)){
+            System.out.print("Guess a number: ");
+            guess = Integer.parseInt(reader.nextLine());
+            count++;
+        }
+    }
+}
