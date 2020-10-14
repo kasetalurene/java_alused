8cb76251a6fb58762786091eb8ab534ec1e176e1
rene.kasetalu <rene.kasetalu@khk.ee>
2020-10-14 12:19:18 +0300

    Ylesanne 16

--- main.java
+++ main.java
@@ -1,15 +1,13 @@
 import java.util.Scanner;
-
 public class main {
-
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         System.out.print("Type a number: ");
         int number = Integer.parseInt(reader.nextLine());
-        if(number >= 0) {
-            System.out.println("The number is positive.");
+        if(number % 2 == 0) {
+            System.out.println("Number " + number + " is even");
         } else {
-            System.out.println("The number is not positive.");
+            System.out.println("Number " + number + " is odd");
         }
     }
 }
