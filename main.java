cf5336b37afcbb1fbcd7b231a538d5e945a29ba4
rene.kasetalu <rene.kasetalu@khk.ee>
2020-10-14 12:31:18 +0300

    Ylesanne 18

--- main.java
+++ main.java
@@ -1,13 +1,22 @@
 import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Type a number: ");
-        int number = Integer.parseInt(reader.nextLine());
-        if(number % 2 == 0) {
-            System.out.println("Number " + number + " is even");
-        } else {
-            System.out.println("Number " + number + " is odd");
+        System.out.print("Type the points [0-60]: ");
+        int points = Integer.parseInt(reader.nextLine());
+        if(points >= 50 && points <= 60) {
+            System.out.println("Grade: 5");
+        } else if(points >= 45 && points <= 49){
+            System.out.println("Grade: 4");
+        } else if(points >= 40 && points <= 44){
+            System.out.println("Grade: 3");
+        } else if(points >= 35 && points <= 39){
+            System.out.println("Grade: 2");
+        } else if(points >= 30 && points <= 34){
+            System.out.println("Grade: 1");
+        } else if(points >= 0 && points <= 29){
+            System.out.println("Failed");
         }
     }
 }
