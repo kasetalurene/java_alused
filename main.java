9f449bfa2e06c7a7378021c84a598f3e1f58b944
rene.kasetalu <rene.kasetalu@khk.ee>
2020-10-14 14:22:03 +0300

    Ylesanne 36

--- main.java
+++ main.java
@@ -2,16 +2,36 @@ import java.util.Scanner;
 
 public class main {
 
+   // private static int sum;
+
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);  
-        System.out.print("Until what? ");
-        int upperNumber = Integer.parseInt(reader.nextLine());
-        int currentNumber = 1;
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Type numbers: ");
         int sum = 0;
-        while (currentNumber <= upperNumber){ 
-            sum += currentNumber;
-            currentNumber++;
+        int countAll = 0;
+        int countEven = 0;
+        int countOdd = 0;
+        double average = 0.0;
+        while(true){
+            int number = Integer.parseInt(reader.nextLine());
+            if(number == -1){
+                break;
+            }
+            countAll++;
+            if(number % 2 == 0){
+                countEven++;
+            } else {
+                countOdd++;
+            }
+            sum += number;
+            average = (double)sum / countAll;
         }
+        System.out.println("Thank you and see you later!");
         System.out.println("Sum is " + sum);
+        System.out.println("How many numbers: " + countAll);
+        System.out.println("How many even numbers: " + countEven);
+        System.out.println("How many odd numbers: " + countOdd);
+        System.out.println("Average: " + average);
     }
-}
+}  
+       
