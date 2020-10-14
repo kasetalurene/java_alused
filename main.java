1ee583088a73e9251256d9c1d6ae8f778544a971
rene.kasetalu <rene.kasetalu@khk.ee>
2020-10-14 13:50:01 +0300

    Ylesanne 29

--- main.java
+++ main.java
@@ -2,18 +2,13 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        int sum = 0;
-        while (true) {
-            int read = Integer.parseInt(reader.nextLine());
-            if (read == 0) {
-                break;
-            } 
-
-            sum = sum + read;
-            System.out.println("Sum now: " + sum); 
-
-        }   
-        System.out.println("Sum in the end: " + sum);
+            int number = 2;
+            while(number <= 100){
+            if(number % 2 == 0){
+                System.out.println(number);
+            }
+            
+            number++;
+        }
     }
 }
