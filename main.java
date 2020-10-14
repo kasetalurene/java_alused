c913d3bf0846b5d1022ec11c75753f458b3222d8
rene.kasetalu <rene.kasetalu@khk.ee>
2020-10-14 12:53:29 +0300

    Ylesanne 21

--- main.java
+++ main.java
@@ -2,13 +2,21 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        System.out.print("How old are you? ");
-        int age = Integer.parseInt(reader.nextLine());
-        if(age > 0 && age <= 120) {
-            System.out.println("OK");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Type a year: ");
+        int year = Integer.parseInt(input.nextLine());
+        boolean leapyear = false;
+        if(year % 100 == 0){
+            if(year % 400 == 0){
+                leapyear = true;
+            }
+        } else if (year % 4 == 0){
+            leapyear = true;
+        }
+        if(leapyear){
+            System.out.println("The year is a leap year.");
         } else {
-            System.out.println("Impossible!");
+            System.out.println("The year is not a leap year.");
         }
     }
 }
