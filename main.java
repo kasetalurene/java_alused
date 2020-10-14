c8370c50112d3d05e1053ef1d00dc055c67ae622
rene.kasetalu <rene.kasetalu@khk.ee>
2020-10-14 14:08:41 +0300

    Ylesanne 32

--- main.java
+++ main.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 
 public class main {
+
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        System.out.print("Up to what number? ");
+        Scanner reader = new Scanner(System.in);  
+        System.out.print("Until what? ");
         int upperNumber = Integer.parseInt(reader.nextLine());
         int currentNumber = 1;
-        while (currentNumber <= upperNumber){
-            System.out.println(currentNumber);
+        int sum = 0;
+        while (currentNumber <= upperNumber){ 
+            sum += currentNumber;
             currentNumber++;
         }
+        System.out.println("Sum is " + sum);
     }
 }
