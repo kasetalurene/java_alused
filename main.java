d537aa38d7738ad5bab023f9d36e12f514737b7c
kasetalurene <rene.kasetalu@khk.ee>
2020-10-15 15:19:16 +0300

    Ylesanne 58

--- main.java
+++ main.java
@@ -1,27 +1,19 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-
 public class main {
-
-public static void main(String[] args) {
-    Scanner reader = new Scanner(System.in);
-
-    ArrayList<String> words = new ArrayList<String>();
-
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        ArrayList<String> words = new ArrayList<String>();
         while (true) {
             System.out.print("Type a word: ");
             String word = reader.nextLine();
-            if (word.isEmpty()) {
+            if (words.contains(word)) {
+                System.out.println("You gave the word " + word + " twice");
                 break;
             } else {
                 words.add(word);
             }
         }
 
-        System.out.println("You typed the following words:");
-        for (String word : words
-        ) {
-            System.out.println(word);
-        }
-    }
+    }    
 }
