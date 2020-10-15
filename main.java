8d5556833ebb91be058ee1555c5af7c6da3050b8
kasetalurene <rene.kasetalu@khk.ee>
2020-10-15 15:44:37 +0300

    Ylesanne 62

--- main.java
+++ main.java
@@ -3,26 +3,27 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class main {
-    
+
+    public static void removeLast(ArrayList<String> list) {
+        list.remove(list.size() - 1);
+    }
+
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        ArrayList<String> words = new ArrayList<String>();
-        while (true) {
-            System.out.print("Type a word: ");
-            String word = reader.nextLine();
-            if (word.isEmpty()) {
-                break;
-            } else {
-                words.add(word);
-            }
-        }
+        ArrayList<String> brothers = new ArrayList<String>();
+        brothers.add("Dick");
+        brothers.add("Henry");
+        brothers.add("Michael");
+        brothers.add("Bob");
 
-        Collections.sort(words);
+        System.out.println("brothers:");
+        System.out.println(brothers);
 
-        System.out.println("You typed the following words:");
-        for (String word : words
-        ) {
-            System.out.println(word);
-        }
+  
+        Collections.sort(brothers);
+
+
+        removeLast(brothers);
+
+        System.out.println(brothers);
     }
 }
