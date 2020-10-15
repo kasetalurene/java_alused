a63cb49cc6d4a3bcbf4bcbd806982fcc326196c3
kasetalurene <rene.kasetalu@khk.ee>
2020-10-15 14:54:47 +0300

    Ylesanne 53

--- main.java
+++ main.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
 
 public class main {
+
     public static void main(String[] args) {
     Scanner reader = new Scanner(System.in);
-    System.out.print("Type your name: ");
-    String name = reader.nextLine();
-    if (name.length() >= 3) {
-        System.out.println(name.charAt(0));
-        System.out.println(name.charAt(1));
-        System.out.println(name.charAt(2));
-        System.out.println(name.charAt(3));
-        }
+    System.out.print("Type a word: ");
+    String word = reader.nextLine();
+    System.out.print("Length of the first part: ");
+    int length = Integer.parseInt(reader.nextLine());
+    System.out.println("Result: " + word.substring(0, length));
+
     }
 }
