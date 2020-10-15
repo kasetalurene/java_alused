169dec439877c02d993a7bf9bd9aef992ed3b433
kasetalurene <rene.kasetalu@khk.ee>
2020-10-15 15:52:26 +0300

    Ylesanne 66

--- main.java
+++ main.java
@@ -1,24 +1,27 @@
+  
 import java.util.ArrayList;
 
 public class main {
 
-    public static double average(ArrayList<Integer> list) {
+    public static int greatest(ArrayList<Integer> list) {
         // write your code here
-        int sum = 0;
-        for (Integer number : list
-        ) {
-            sum += number;
+        int greatestNumber = list.get(0);
+        for (Integer number : list) {
+            if (number > greatestNumber) {
+                greatestNumber = number;
+            }
         }
-        return (double) sum / list.size();
+        return greatestNumber;
     }
+
 
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<Integer>();
-        list.add(3);
+        list.add(11);
         list.add(2);
         list.add(7);
         list.add(2);
 
-        System.out.println("The average is: " + average(list));
+        System.out.println("The greatest number is: " + greatest(list));
     }
 }
