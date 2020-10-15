4e3f506952f8536cbc3ec3d179c2af24fde28591
kasetalurene <rene.kasetalu@khk.ee>
2020-10-15 14:47:34 +0300

    Ylesanne 50

--- main.java
+++ main.java
@@ -1,18 +1,15 @@
 import java.util.Scanner;
 
 public class main {
-
-    public static double average(int number1, int number2, int number3, int number4) {
-        // write your code here
-        int sum = number1 + number2 + number3 + number4;
-        double average = (double) sum / 4;
-        return average;
-
-    }
-
     public static void main(String[] args) {
-
-    double answer = average(4, 3, 6, 1);
-    System.out.println("average: " + answer);
-}
+    Scanner reader = new Scanner(System.in);
+    System.out.print("Type your name: ");
+    String name = reader.nextLine();
+    if (name.length() >= 3) {
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(1));
+        System.out.println(name.charAt(2));
+        System.out.println(name.charAt(3));
+        }
+    }
 }
