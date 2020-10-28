515da59ea6ca2e7fc6ebc75f4d17aa5b4a743c76
rene.kasetalu <rene.kasetalu@khk.ee>
2020-10-28 10:19:39 +0200

    Ylesanne 77

--- main.java
+++ main.java
@@ -2,12 +2,20 @@ import java.util.Scanner;
 
 public class main {
      public static void main(String[] args) {
-        Menu cafee = new Menu();
-        cafee.addMeal("Hamburger");
-        cafee.addMeal("Fish'n'Chips");
-        cafee.addMeal("Sauerkraut");
-    cafee.printMeals();
-    cafee.clearMenu();
-    cafee.printMeals();
-     }
+        LyyraCard cardPekka = new LyyraCard(20);
+        LyyraCard cardBrian = new LyyraCard(30);
+        cardPekka.payGourmet();
+        cardBrian.payEconomical();
+        System.out.println("Pekka: " + cardPekka);
+        System.out.println("Brian: " + cardBrian);
+        cardPekka.loadMoney(20);
+        cardBrian.payGourmet();
+        System.out.println("Pekka: " + cardPekka);
+        System.out.println("Brian: " + cardBrian);
+        cardPekka.payEconomical();
+        cardPekka.payEconomical();
+        cardBrian.loadMoney(50);
+        System.out.println("Pekka: " + cardPekka);
+        System.out.println("Brian: " + cardBrian);
+   }
 }
