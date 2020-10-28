050b78f1e6cc5bb4192e81677faa5080e105af4a
rene.kasetalu <rene.kasetalu@khk.ee>
2020-10-28 11:24:23 +0200

    Ylesanne 79

--- main.java
+++ main.java
@@ -1,35 +1,24 @@
 import java.util.Scanner;
 
 public class main { public static void main(String[] args) throws Exception {
-
-    BoundedCounter seconds = new BoundedCounter(59);
-    BoundedCounter minutes = new BoundedCounter(59);
-    BoundedCounter hours = new BoundedCounter(23);
+    NumberStatistics allNumbers = new NumberStatistics();
+    NumberStatistics evenNumbers = new NumberStatistics();
+    NumberStatistics oddNumbers = new NumberStatistics();
 
     Scanner reader = new Scanner(System.in);
-
-    System.out.print("seconds: ");
-    int s = Integer.parseInt(reader.nextLine());
-    System.out.print("minutes: ");
-    int m = Integer.parseInt(reader.nextLine());
-    System.out.print("hours: ");
-    int h = Integer.parseInt(reader.nextLine());
-
-    seconds.setValue(s);
-    minutes.setValue(m);
-    hours.setValue(h);
-    
-    while ( true ) {
-        System.out.println( hours + ":" + minutes + ":" + seconds );
-        Thread.sleep(1000);
-        seconds.next();
-        if(seconds.getValue() == 0){
-            minutes.next();
-            if(minutes.getValue() == 0){
-                hours.next();
-            }
+    System.out.println("Type numbers:");
+    int number = Integer.parseInt(reader.nextLine());
+    while(number != -1){
+        allNumbers.addNumber(number);
+        if(number % 2 == 0){
+            evenNumbers.addNumber(number);
+        } else {
+            oddNumbers.addNumber(number);
         }
+        number = Integer.parseInt(reader.nextLine());
     }
+    System.out.println("sum: " + allNumbers.sum());
+    System.out.println("sum of even: " + evenNumbers.sum());
+    System.out.println("sum of odd: " + oddNumbers.sum());
 }
-
 }
