5ff7a25f06565bfe3a5201dbc7fd45843e00cc37
rene.kasetalu <rene.kasetalu@khk.ee>
2020-10-28 11:41:42 +0200

    Ylesanne 81

--- main.java
+++ main.java
@@ -1,24 +1,10 @@
 import java.util.Scanner;
 
 public class main { public static void main(String[] args) throws Exception {
-    NumberStatistics allNumbers = new NumberStatistics();
-    NumberStatistics evenNumbers = new NumberStatistics();
-    NumberStatistics oddNumbers = new NumberStatistics();
-
-    Scanner reader = new Scanner(System.in);
-    System.out.println("Type numbers:");
-    int number = Integer.parseInt(reader.nextLine());
-    while(number != -1){
-        allNumbers.addNumber(number);
-        if(number % 2 == 0){
-            evenNumbers.addNumber(number);
-        } else {
-            oddNumbers.addNumber(number);
-        }
-        number = Integer.parseInt(reader.nextLine());
-    }
-    System.out.println("sum: " + allNumbers.sum());
-    System.out.println("sum of even: " + evenNumbers.sum());
-    System.out.println("sum of odd: " + oddNumbers.sum());
+    PasswordRandomizer randomizer = new PasswordRandomizer(13);
+    System.out.println("Password: " + randomizer.createPassword());
+    System.out.println("Password: " + randomizer.createPassword());
+    System.out.println("Password: " + randomizer.createPassword());
+    System.out.println("Password: " + randomizer.createPassword());
 }
 }
