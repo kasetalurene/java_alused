efff7b4f2affb0d152b2dcec3c3e6c029b2b48ac
rene.kasetalu <rene.kasetalu@khk.ee>
2020-10-28 10:00:52 +0200

    Ylesanne 75

---
+++ DecreasingCounter.java
@@ -0,0 +1,14 @@
+public class DecreasingCounter {
+
+    private int value;
+    public DecreasingCounter(int valueAtStart) {
+        this.value = valueAtStart;
+    }
+    public void printValue() {
+        System.out.println("value: " + this.value);
+    }
+    public void decrease() {
+        this.value--;
+    }
+} 
+
--- main.java
+++ main.java
@@ -1,35 +1,12 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class main {
-
-    public static String reverse(String text) {
-        // write your code here
-        int i = text.length() - 1; // last symbol index
-        String reverseText = "";
-        while (i >= 0) {
-            reverseText += text.charAt(i);
-            i--;
-        }
-        return reverseText;
-    }
-    public static boolean palindrome(String text) {
-        // write your code here
-        if (text.equals(reverse(text))) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-
-        System.out.println("Type a text: ");
-        String text = reader.nextLine();
-        if (palindrome(text)) {
-            System.out.println("The text is a palindrome!");
-        } else {
-            System.out.println("The text is not a palindrome!");
-        }
-    }
+     public static void main(String[] args) {
+        DecreasingCounter counter = new DecreasingCounter(10);
+        counter.printValue();
+        counter.decrease();
+        counter.printValue();
+        counter.decrease();
+        counter.printValue();
+   }
 }
