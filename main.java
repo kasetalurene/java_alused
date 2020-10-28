933091cda962fa619e370853120ebfb4d29b7635
rene.kasetalu <rene.kasetalu@khk.ee>
2020-10-28 10:40:24 +0200

    Ylesanne 78

--- main.java
+++ main.java
@@ -1,21 +1,35 @@
 import java.util.Scanner;
 
-public class main {
-     public static void main(String[] args) {
-        LyyraCard cardPekka = new LyyraCard(20);
-        LyyraCard cardBrian = new LyyraCard(30);
-        cardPekka.payGourmet();
-        cardBrian.payEconomical();
-        System.out.println("Pekka: " + cardPekka);
-        System.out.println("Brian: " + cardBrian);
-        cardPekka.loadMoney(20);
-        cardBrian.payGourmet();
-        System.out.println("Pekka: " + cardPekka);
-        System.out.println("Brian: " + cardBrian);
-        cardPekka.payEconomical();
-        cardPekka.payEconomical();
-        cardBrian.loadMoney(50);
-        System.out.println("Pekka: " + cardPekka);
-        System.out.println("Brian: " + cardBrian);
-   }
+public class main { public static void main(String[] args) throws Exception {
+
+    BoundedCounter seconds = new BoundedCounter(59);
+    BoundedCounter minutes = new BoundedCounter(59);
+    BoundedCounter hours = new BoundedCounter(23);
+
+    Scanner reader = new Scanner(System.in);
+
+    System.out.print("seconds: ");
+    int s = Integer.parseInt(reader.nextLine());
+    System.out.print("minutes: ");
+    int m = Integer.parseInt(reader.nextLine());
+    System.out.print("hours: ");
+    int h = Integer.parseInt(reader.nextLine());
+
+    seconds.setValue(s);
+    minutes.setValue(m);
+    hours.setValue(h);
+    
+    while ( true ) {
+        System.out.println( hours + ":" + minutes + ":" + seconds );
+        Thread.sleep(1000);
+        seconds.next();
+        if(seconds.getValue() == 0){
+            minutes.next();
+            if(minutes.getValue() == 0){
+                hours.next();
+            }
+        }
+    }
+}
+
 }
